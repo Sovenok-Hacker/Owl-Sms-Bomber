713db1989104cf850f3f3a85defb56579801361a
Artem <79601745+Sovenok-Hacker@users.noreply.github.com>
2022-02-10 11:45:14 +0300

    Add files via upload

---
+++ bomber.sh
@@ -0,0 +1,6 @@
+apk install python3 git python3-pip
+pip3 install --upgrade pip
+git clone https://github.com/Sovenok-Hacker/Owl-Sms-Bomber.git
+cd Owl-Sms-Bomber
+pip3 install -r req*
+python3 SovaSmsBomber.py
